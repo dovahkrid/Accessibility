d7186a834346229f74e8c7ff6174817bd6765f9a
litan <litan@dianxinos.com>
2014-07-24 18:13:52 +0800

    initial commit for password

--- src/com/litan/accessibilitytest/AccessibilityTestService.java
+++ src/com/litan/accessibilitytest/AccessibilityTestService.java
@@ -4,104 +4,125 @@ package com.litan.accessibilitytest;
 import java.util.List;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Build;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
-import android.widget.Toast;
 
 public class AccessibilityTestService extends AccessibilityService {
     private static final String TAG = "litan";
-    private boolean mIsOkClicked;
-    private boolean mIsStopClicked;
-    private Toast mToast;
+    private boolean m163Clicked;
+    private boolean mPasswordWaitingForFocusd;
 
     private void log(AccessibilityNodeInfo node, int level) {
+        if (node == null) {
+            return;
+        }
         logd(level + " node:" + node.getChildCount() + node.getViewIdResourceName());
-        for (int i = 0;i<node.getChildCount();i++) {
+        for (int i = 0; i < node.getChildCount(); i++) {
             AccessibilityNodeInfo n = node.getChild(i);
             log(n, level + 1);
         }
     }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         logi("onAccessibilityEvent:" + event);
         AccessibilityNodeInfo source = event.getSource();
         if (source == null) {
+            logv("No source node");
             return;
         } else {
-            //log(source, 0);
-        }
-//        if (true)
-//        return;
-        // just test code
-//        if ("com.example.androidtest".equals(event.getPackageName())) {
-//            log(source, 0);
-//        } else {
-//            return;
-//        }
-        // read chrome browser addr
-        if ("com.android.chrome".equals(event.getPackageName())) {
             log(source, 0);
-//            String str = "";
-//            if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED) {
-//                str = "TEXT_SELECTION_CHANGED:" + event.getText().toString();
-//            } else if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
-//                str = "TEXT_CHANGED:" + event.getText().toString();
-//            }
-//            if (!TextUtils.isEmpty(str)) {
-//                if (mToast == null) {
-//                    mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
-//                }
-//                mToast.setText( str);
-//                logd(str);
-//                mToast.show();
-//            }
-            return;
-        }
-        if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            return;
-        }
-        logd("AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED");
-        if (!"com.android.settings".equals(event.getPackageName())) {
-            logd("Non settings");
-            return;
-        }
-        if ("com.android.settings.applications.InstalledAppDetailsTop".equals(event.getClassName())) {
-            logd("enter InstalledAppDetailsTop");
-            AccessibilityNodeInfo node = findNode(source, new String[] {
-                    "com.android.settings:id/left_button",
-                    "com.android.settings:id/force_stop_button"
-            }, "强行停止");
-            if (node != null) {
-                if (mIsOkClicked) {
-                    mIsOkClicked = false;
-                    performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
-                    return;
-                }
-                if (node.isEnabled()) {
-                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    logi("perform 强行停止");
-                    mIsStopClicked = true;
-                }
+            if (AccessibilityEvent.TYPE_VIEW_CLICKED == event.getEventType()
+                    && "com.netease.mobimail:id/domain_163".equals(source.getViewIdResourceName())) {
+                m163Clicked = true;
                 return;
             }
-        } else if ("android.app.AlertDialog".equals(event.getClassName())) {
-            logd("enter AlertDialog");
-            AccessibilityNodeInfo node = findNode(source, new String[] {
-                    "android:id/button1"
-            }, "确定");
-            if (node != null) {
-                if (mIsStopClicked) {
-                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    logi("perform 确定");
-                    mIsOkClicked = true;
-                    mIsStopClicked = false;
+            if (AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED == event.getEventType()) {
+                if ("com.netease.mobimail.activity.LoginActivity".equals(event.getClassName())) {
+                    m163Clicked = false;
+                    return;
+                } else {
+                    AccessibilityNodeInfo account = findNode(source, new String[] {
+                        "com.netease.mobimail:id/editor_email"
+                    }, "");
+                    AccessibilityNodeInfo password = findNode(source, new String[] {
+                        "com.netease.mobimail:id/editor_password"
+                    }, "");
+                    if (m163Clicked && account != null && password != null) {
+                        ClipboardManager mgr = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                        mgr.setText("liyan12167");
+                        account.performAction(AccessibilityNodeInfo.ACTION_PASTE);
+                        password.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
+                        mPasswordWaitingForFocusd = true;
+                        return;
+                    }
+                }
+                // AccessibilityNodeInfo node = findNode(source, new
+                // String[]{"com.netease.mobimail:id/domain_163"}, "");
+                // if (node != null) {
+                //
+                // }
+                return;
+            } else if (AccessibilityEvent.TYPE_VIEW_FOCUSED == event.getEventType()) {
+                if (mPasswordWaitingForFocusd
+                        && "com.netease.mobimail:id/editor_password".equals(source
+                                .getViewIdResourceName())) {
+                    ClipboardManager mgr = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    mgr.setText("Jz5855657");
+                    source.performAction(AccessibilityNodeInfo.ACTION_PASTE);
+                    mPasswordWaitingForFocusd = false;
                 }
             }
-            return;
         }
+        // if (event.getEventType() !=
+        // AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+        // return;
+        // }
+        // logd("AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED");
+        // if (!"com.android.settings".equals(event.getPackageName())) {
+        // logd("Non settings");
+        // return;
+        // }
+        // if
+        // ("com.android.settings.applications.InstalledAppDetailsTop".equals(event.getClassName()))
+        // {
+        // logd("enter InstalledAppDetailsTop");
+        // AccessibilityNodeInfo node = findNode(source, new String[] {
+        // "com.android.settings:id/left_button",
+        // "com.android.settings:id/force_stop_button"
+        // }, "强行停止");
+        // if (node != null) {
+        // if (mIsOkClicked) {
+        // mIsOkClicked = false;
+        // performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+        // return;
+        // }
+        // if (node.isEnabled()) {
+        // node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        // logi("perform 强行停止");
+        // mIsStopClicked = true;
+        // }
+        // return;
+        // }
+        // } else if ("android.app.AlertDialog".equals(event.getClassName())) {
+        // logd("enter AlertDialog");
+        // AccessibilityNodeInfo node = findNode(source, new String[] {
+        // "android:id/button1"
+        // }, "确定");
+        // if (node != null) {
+        // if (mIsStopClicked) {
+        // node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        // logi("perform 确定");
+        // mIsOkClicked = true;
+        // mIsStopClicked = false;
+        // }
+        // }
+        // return;
+        // }
     }
 
     private AccessibilityNodeInfo findNode(AccessibilityNodeInfo source, String[] ids, String text) {
@@ -130,18 +151,11 @@ public class AccessibilityTestService extends AccessibilityService {
     @Override
     public void onInterrupt() {
         logi("onInterrupt");
-
     }
 
     @Override
     public void onServiceConnected() {
         logi("onServiceConnected");
-        // AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        // info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
-        // | AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-        // info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
-        // info.notificationTimeout = 80;
-        // setServiceInfo(info);
     }
 
     private static void logd(String msg) {
@@ -154,5 +168,9 @@ public class AccessibilityTestService extends AccessibilityService {
 
     private static void logi(String msg) {
         Log.i(TAG, msg);
+    }
+
+    private static void logv(String msg) {
+        Log.v(TAG, msg);
     }
 }
