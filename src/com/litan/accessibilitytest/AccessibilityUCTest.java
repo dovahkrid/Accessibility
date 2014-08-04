59397712e30d1d6b6b6b65fd97353bbc739c5106
litan <litan@dianxinos.com>
2014-08-04 18:52:39 +0800

    add uc test

---
+++ src/com/litan/accessibilitytest/AccessibilityUCTest.java
@@ -0,0 +1,20 @@
+package com.litan.accessibilitytest;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class AccessibilityUCTest extends AccessibilityService {
+
+	@Override
+	public void onAccessibilityEvent(AccessibilityEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onInterrupt() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
